565a3c671a1db63530bed89cda5857364113b4fe
Gathabuer <do_16do@wp.pl>
2025-11-14 23:00:07 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,18 @@
+const dot = document.querySelector('.cursor-dot');
+
+let mouseX = 0, mouseY = 0;
+let dotX = 0, dotY = 0;
+
+document.addEventListener('mousemove', e => {
+  mouseX = e.clientX;
+  mouseY = e.clientY;
+});
+
+function animate() {
+  dotX += (mouseX - dotX) * 0.1;
+  dotY += (mouseY - dotY) * 0.1;
+  dot.style.transform = `translate(${dotX}px, ${dotY}px)`;
+  requestAnimationFrame(animate);
+}
+
+animate();
